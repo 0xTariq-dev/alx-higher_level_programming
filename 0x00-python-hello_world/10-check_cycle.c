35542dc16dd5e745247f08637f6d049c7c8ab20c
0xTariq-dev <tariqmohsen222@gmail.com>
2023-09-04 14:53:14 -0700

    feat: Add a function to check if a list is circular or not

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Checks if a linked list is circular or not.
+ * @list: A pointer to the list struct.
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *current, *new;
+
+	if (!list)
+		return (0);
+
+	current = list;
+	new = list->next;
+
+	while (current && new && new->next)
+	{
+		if (current == new)
+			return (1);
+		current = current->next;
+		new = new->next->next;
+	}
+	return (0);
+}
