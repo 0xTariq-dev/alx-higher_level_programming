5402936091f660813663a8e0e81669a37ffc7121
0xTariq-dev <tariqmohsen222@gmail.com>
2023-09-06 16:04:47 -0700

    fix: Behavior when negative index is passed

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    new_str = str[:n] + str[n+1:]
-    return new_str
+    if n >= 0:
+        new_str = str[:n] + str[n + 1:]
+        return new_str
+    else:
+        return str
