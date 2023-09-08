45b8c9e31052704865a71a9d8290eb324e035e9f
0xTariq-dev <tariqmohsen222@gmail.com>
2023-09-07 20:06:10 -0700

    feat: Add calculator program

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
 if __name__ == '__main__':
-    a = 10
-    b = 5
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+    import sys
+    from calculator_1 import add, sub, mul, div
+    n = len(sys.argv) - 1
+    operators = {'+', '-', '*', '/'}
+    if n == 3:
+        a, operator, b = int(sys.argv[1]), sys.argv[2], int(sys.argv[3])
+    elif n != 3:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        sys.exit(1)
+    if operator not in operators:
+        print('Unknown operator. Available operators: +, -, * and /')
+        sys.exit(1)
+    if operator == '+':
+        print('{} {} {} = {}'.format(a, operator, b, add(a, b)))
+    elif operator == '-':
+        print('{} {} {} = {}'.format(a, operator, b, sub(a, b)))
+    elif operator == '*':
+        print('{} {} {} = {}'.format(a, operator, b, mul(a, b)))
+    elif operator == '/':
+        print('{} {} {} = {}'.format(a, operator, b, div(a, b)))
