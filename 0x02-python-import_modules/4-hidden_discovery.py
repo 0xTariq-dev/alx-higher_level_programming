8b13208c9a17a21e13941f90d50d891d38a12d88
0xTariq-dev <tariqmohsen222@gmail.com>
2023-09-07 19:11:25 -0700

    feat: Add program that prints file names that doesn't start with __ from a compiled module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    import sys
-    sum_arg = 0
-    for i in range(1, len(sys.argv)):
-        sum_arg += int(sys.argv[i])
-    print(sum_arg)
+    import hidden_4
+    file_names = dir(hidden_4)
+    for name in sorted(file_names):
+        if not name.startswith('__'):
+            print(name)
