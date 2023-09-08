7bb4ad9d1b1560f5333554b2c51ec2f79e099c49
0xTariq-dev <tariqmohsen222@gmail.com>
2023-09-07 17:28:40 -0700

    fix: Stop argument printing program from executing if imported

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 import sys
-n = len(sys.argv)
-string = '{} {}{}'.format(
-        n,
-        'argument' if n == 1 else 'arguments',
-        '.' if n == 0 else ':'
-)
-print(string)
-if n != 0:
-    for i in range(1, n):
-        print('{}: {}'.format(i, sys.argv[i]))
+if __name__ == 'main':
+    
+    n = len(sys.argv)
+    string = '{} {}{}'.format(
+            n,
+            'argument' if n == 1 else 'arguments',
+            '.' if n == 0 else ':'
+    )
+    print(string)
+    if n != 0:
+        for i in range(1, n):
+            print('{}: {}'.format(i, sys.argv[i]))
