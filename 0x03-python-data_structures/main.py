91638fe77d8806045ec0bfa11d8c3dea7265f23a
0xTariq-dev <tariqmohsen222@gmail.com>
2023-09-09 15:13:16 -0700

    refactor: 1-element_at.py

--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-replace_in_list = __import__('2-replace_in_list').replace_in_list
+element_at = __import__('1-element_at').element_at
 
 my_list = [1, 2, 3, 4, 5]
 idx = 3
-new_element = 9
-new_list = replace_in_list(my_list, idx, new_element)
+print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
 
-print(new_list)
-print(my_list)
