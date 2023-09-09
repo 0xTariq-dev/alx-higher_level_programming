1a55cd964beb9bd6dc265228a3ff5d0e45e8fa0f
0xTariq-dev <tariqmohsen222@gmail.com>
2023-09-09 09:32:05 -0700

    func: Replace element at specific index in a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        return None
-    if idx >= len(my_list):
+    if len(my_list) <= idx < 0:
         return None
     else:
         return my_list[idx]
---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if len(my_list) <= idx < 0:
+        return my_list
+    else:
+        my_list[idx] = element
