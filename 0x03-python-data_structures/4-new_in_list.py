758ca92482f9233b19d84404ae22c75ab1a220b7
0xTariq-dev <tariqmohsen222@gmail.com>
2023-09-12 09:57:01 -0700

    fix: Replace in a copy function

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     new = my_list.copy()
-    if idx >= 0 or idx < len(my_list):
-        new[idx] = element
+    if idx < 0 or idx >= len(my_list):
         return new
     else:
+        new[idx] = element
         return new
