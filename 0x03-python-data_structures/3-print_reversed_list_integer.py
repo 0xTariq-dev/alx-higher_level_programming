9075839dbfa99408507ae5a6bcf004ba6a98ae0d
0xTariq-dev <tariqmohsen222@gmail.com>
2023-09-12 09:52:52 -0700

    fix: Replace in a copy and print in reverse functions

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    n = len(my_list) - 1
-    for i in range(n, -1, -1):
-        print('{:d}'.format(my_list[i]))
+    if my_list:
+        my_list.reverse()
+        for i in range(len(my_list)):
+            print('{:d}'.format(my_list[i]))
