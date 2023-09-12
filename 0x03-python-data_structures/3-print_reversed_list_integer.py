86276c2ff2bf3973405afb9face43c0ae50bf169
0xTariq-dev <tariqmohsen222@gmail.com>
2023-09-12 09:28:34 -0700

    fix: Output when list equals None

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     n = len(my_list) - 1
-    if my_list:
-        for i in range(n, -1, -1):
-            print('{}'.format(my_list[i]))
-    else:
-        return
+    for i in range(n, -1, -1):
+        print('{:d}'.format(my_list[i]))
