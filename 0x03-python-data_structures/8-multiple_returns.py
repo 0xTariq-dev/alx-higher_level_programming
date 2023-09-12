bd4569f63c0749ee5f451fbd8334ab4ceedc36f5
0xTariq-dev <tariqmohsen222@gmail.com>
2023-09-12 15:32:02 -0700

    fix: if sentence is empty case output

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tuple = (len(sentence), sentence[0])
+    tuple = (len(sentence), sentence[0]) if sentence != '' else (0, None)
     return tuple
