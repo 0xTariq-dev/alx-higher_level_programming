bac23e5a67ebb785d93efd496569bde5da29a009
0xTariq-dev <tariqmohsen222@gmail.com>
2023-09-13 16:07:44 -0700

    fix: if key doesn't exist return the same dictionary

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if key in a_dictionary:
+    if a_dictionary.get(key) is not None:
         a_dictionary.pop(key, None)
     return a_dictionary 
