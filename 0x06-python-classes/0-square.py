e0bc34ea5843294a9838910c24f456ff1c917fbf
0xTariq-dev <tariqmohsen222@gmail.com>
2023-09-26 19:40:15 -0700

    fix: Empty class square documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """0-square Module:
     An empty module that defines square"""
+
+
 class Square:
     """A sqaure is the product of multiblying a number by itself."""
     pass
