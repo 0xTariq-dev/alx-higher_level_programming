d93fcfdb10556281d7f082326ba8dcfd248b9c3f
0xTariq-dev <tariqmohsen222@gmail.com>
2023-10-02 16:43:21 -0700

    feat: Add private instances width and height to rectangle class

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""1-Rectangle Module:
+    A module that defines rectangle"""
+
+
+class Rectangle:
+    """
+    A Rectangle is the product of multiblying a length and width of
+        an area.
+    """
+
+
+    def __init__(self, width=0, height=0):
+        ''' Instantiation method of rectangle class
+        Args:
+            width (int): The width of the rectangle
+            height (int): The height of the rectangle
+        '''
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        ''' Property of the width value
+        
+        Args:
+            value: The width value.
+            width must be an integer and greater than or equal to zero.
+
+        Returns: The width.
+        '''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        ''' Property of the height value
+        
+        Args:
+            value: The height value.
+            height must be an integer and greater than or equal to zero.
+
+        Returns: The height.
+        '''
+        return self.__height
+
+    @width.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        else:
+            self.__height = value
