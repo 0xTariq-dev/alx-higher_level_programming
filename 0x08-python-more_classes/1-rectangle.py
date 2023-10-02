553a916f0a6a5fdbda427593dba57aa2671d9c9e
0xTariq-dev <tariqmohsen222@gmail.com>
2023-10-02 16:46:41 -0700

    feat: Add private instances width and height to rectangle class

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -34,7 +34,7 @@ class Rectangle:
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
-        elif value < 0:
+        if value < 0:
             raise ValueError('width must be >= 0')
         else:
             self.__width = value
