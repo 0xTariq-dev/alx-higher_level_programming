724266dc97eb4deb6267b40780d9fc435c0d0ed7
0xTariq-dev <tariqmohsen222@gmail.com>
2023-10-02 17:32:24 -0700

    feat: Add area and perimeter calculation methods to rectangle class

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""1-Rectangle Module:
-    A module that defines rectangle"""
+"""1-Rectangle Module: A module that defines rectangle"""
 
 
 class Rectangle:
@@ -56,4 +55,4 @@ class Rectangle:
         """
         if self.__width == 0 or self.__height == 0:
             return 0
-        return (self.__width * 2) + (self.__height * 2)
+        return 2 * (self.__width + self.__height)
