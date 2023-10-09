c4dabc223b447c8a6912b4bff22ea710b21d3351
0xTariq-dev <tariqmohsen222@gmail.com>
2023-10-09 16:58:41 -0700

    feat: Add Exception to BaseGeometry Class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines class BaseGeometry"""
+
+
+class BaseGeometry:
+    """BaseGeometry:
+    raises an Exception with the message area() is not implemented.
+    """
+    def area(self):
+        """area:
+        raise Exception
+        """
+        raise Exception('area() is not implemented')
