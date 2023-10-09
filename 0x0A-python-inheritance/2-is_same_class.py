85456de868401112a019ac5d59d7bbb104b898dc
0xTariq-dev <tariqmohsen222@gmail.com>
2023-10-09 16:35:15 -0700

    fix: pycodestyle

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """Defines is_same_class function that returns true if it's same class only."""
 
+
 def is_same_class(obj, a_class):
-	"""is_same_class:
-	Args:
-		a_class: the class to check it's type.
-	Returns:
-		True if it's same class otherwise false.
-	"""
-	if type(obj) is a_class:
-		return True
-	else:
-		return False
+    """is_same_class:
+    Args:
+    a_class: the class to check it's type.
+    Returns:
+    True if it's same class otherwise false.
+    """
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
