78dc2e0dac09e6f485267aa9e26bb29d6d5d50db
0xTariq-dev <tariqmohsen222@gmail.com>
2023-10-09 16:30:47 -0700

    feat: Add is_same_class function

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines is_same_class function that returns true if it's same class only."""
+
+def is_same_class(obj, a_class):
+	"""is_same_class:
+	Args:
+		a_class: the class to check it's type.
+	Returns:
+		True if it's same class otherwise false.
+	"""
+	if type(obj) is a_class:
+		return True
+	else:
+		return False
