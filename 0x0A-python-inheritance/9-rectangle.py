0e2fc15046a44450ffa74d5c6732f5fa1c83921f
0xTariq-dev <tariqmohsen222@gmail.com>
2023-10-10 00:38:25 -0700

    fix: str method for class Rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -15,9 +15,9 @@ class Rectangle(BaseGeometry):
             width (int): The width of the new Rectangle.
             height (int): The height of the new Rectangle.
         """
-        self.integer_validator('width', width)
+        super().integer_validator('width', width)
         self.__width = width
-        self.integer_validator('height', height)
+        super().integer_validator('height', height)
         self.__height = height
 
     def area(self):
@@ -26,6 +26,6 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """Defins the print() and str() methods behavior"""
-        description = "[" + str(self.__class__.__name__) + "]"
-        description += str(self.__width) + "/" + str(self.__height)
+        description = "[" + str(self.__class__.__name__) + "] "\
+            + str(self.__width) + "/" + str(self.__height)
         return description
