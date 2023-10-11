3e53608bcd3120df47011ec37399d47ddb111d37
0xTariq-dev <tariqmohsen222@gmail.com>
2023-10-10 19:58:43 -0700

    feat: Add pascal triangle function

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defines pascal_traingle method"""
+
+
+def pascal_triangle(n):
+    """returns a list of lists of integers representing the Pascal’s
+        triangle of n.
+
+    Args:
+        n (int): The number to get pascal's traingle for.
+    """
+    if n <= 0:
+        return []
+
+    pascal = [[1]]
+    while len(pascal) != n:
+        triangle = pascal[-1]
+        temp = [1]
+        for x in range(len(triangle) - 1):
+            temp.append(triangle[x] + triangle[x + 1])
+        temp.append(1)
+        pascal.append(temp)
+    return pascal
+
+
