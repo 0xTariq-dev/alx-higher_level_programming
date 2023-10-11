a917017d3e9a01629157bbed16a8d8675843838c
0xTariq-dev <tariqmohsen222@gmail.com>
2023-10-10 17:01:17 -0700

    feat: Add read_file method

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines read_file method"""
+
+
+def read_file(filename=""):
+    """Reads a text file encoded in 'UTF8' and prints it to stdout
+    Args:
+        filename (filestream): The file name to open.
+    """
+    with open(filename, mode='r', encoding='UTF8') as f:
+        print(f.read())
