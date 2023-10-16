c563f045cf7d9a66ac1301e59318453757290dbd
0xTariq-dev <tariqmohsen222@gmail.com>
2023-10-16 11:46:52 -0700

    feat: Update the Base class with JSON encoding and JSON file creating methods

--- 0x0C-python-almost_a_circle/main.py
+++ 0x0C-python-almost_a_circle/main.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-""" 13-main """
-from models.square import Square
+""" 15-main """
+from models.rectangle import Rectangle
 
 if __name__ == "__main__":
 
-    s1 = Square(10, 2, 1)
-    print(s1)
-    s1_dictionary = s1.to_dictionary()
-    print(s1_dictionary)
-    print(type(s1_dictionary))
+    r1 = Rectangle(10, 7, 2, 8)
+    r2 = Rectangle(2, 4)
+    Rectangle.save_to_file([r1, r2])
 
-    s2 = Square(1, 1)
-    print(s2)
-    s2.update(**s1_dictionary)
-    print(s2)
-    print(s1 == s2)
+    with open("Rectangle.json", "r") as file:
+        print(file.read())
--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Defines Base Model"""
+import json
 
 
 class Base:
@@ -24,3 +25,29 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list of dictionaries
+
+        Args:
+            list_dictionaries (list): list of dicts for instances.
+        """
+        if list_dictionaries == [] or list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file
+
+        Args:
+            list_obj (list): list of objects to write to the file.
+        """
+        json_file = cls.__name__ + ".json"
+        with open(json_file, "w") as j:
+            if list_objs is None:
+                j.write("[]")
+            else:
+                dicts = [obj.to_dictionary() for obj in list_objs]
+                j.write(Base.to_json_string(dicts))
