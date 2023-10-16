3f9dbe2b256bdbb17dbe9b827f0d291d70fcaf27
0xTariq-dev <tariqmohsen222@gmail.com>
2023-10-16 10:50:59 -0700

    feat: Add Square model

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Defines Square Module"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square:
+    Inherits from Rectangle class
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Instantiation Method
+        Args:
+            size (int): The size of the new square.
+            x (int)(default = 0)
+            y (int)(default = 0)
+        """
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Get the size value"""
+        return self.width
+
+    @size.setter
+    def size (self, value):
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Returns a string representation of the square instance"""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
