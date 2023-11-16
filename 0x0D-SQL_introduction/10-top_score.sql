cb655a1823891cc6889a1ce5536a4dbccff31e2c
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2023-11-16 19:42:21 +0200

    feat: Add script to list all the records in second_table sorted by score in descending order.

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Script that lists all the records in table second_table in the database hbtn_0c_0
+-- in MySQL server and adds multiple rows in it.
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
