fc134cdfeae32f6e4a3f4ea240399fb7d1ce1711
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2023-11-16 19:34:30 +0200

    feat: Add script that creates a table second_table in the database hbtn_0c_0 with multiple rows.

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,12 @@
+-- Script that Creates a table second_table in the database hbtn_0c_0
+-- in MySQL server and adds multiple rows in it.
+CREATE TABLE IF NOT EXISTS `second_table` (
+	`id` INT,
+	`name` VARCHAR(256),
+	`score` INT
+);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
+
