c366a7446544f5abe93163df1449abf1a856be86
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2023-11-16 19:09:30 +0200

    feat: Add a script that counts the occurance of rows where id value is 89

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- Script that counts the number of records where id=89 in the table first_table into the database
+-- hbtn_0c_0 in your MySQL server.
+SELECT COUNT(*) FROM `first_table` WHERE id=89;
