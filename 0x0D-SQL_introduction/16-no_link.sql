ffbca6a559eaf12bd769c7849c4cb52e7ffa2d1f
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2023-11-16 20:18:05 +0200

    feat: Add script that lists all records in second_table that has name in descending order.

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+-- Script that lists all the records with a name in the table
+-- second_table of the database hbtn_0c_0 in your MySQL server.
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
