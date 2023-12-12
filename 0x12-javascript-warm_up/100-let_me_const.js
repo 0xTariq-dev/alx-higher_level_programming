bd3319d767fcd285f7b32eb249ac3b477aca0782
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2023-12-12 22:56:53 +0200

    feat: Script that excutes another function x times.

--- 0x12-javascript-warm_up/100-let_me_const.js
+++ 0x12-javascript-warm_up/100-let_me_const.js
@@ -1,2 +1,3 @@
 #!/usr/bin/node
+// Script to change the variable myVar to 333.
 myVar = 333;
---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// Script that adds two numbers.
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) { theFunction(); }
+};
