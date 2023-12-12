15b01dc3439c09b232f07908894ba21075ef5677
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2023-12-12 20:03:48 +0200

    feat: Script to print a square of a given size.

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Script that print a square of a given size.
+if (!isNaN(process.argv[2])) {
+  const size = parseInt(process.argv[2]);
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+} else {
+  console.log('Missing size');
+}
