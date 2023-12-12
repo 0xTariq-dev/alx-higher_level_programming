4ae8ab6e7ee8641c792041436bbc92cefc7141a7
0xTariq-dev <tariqmohsen222@gmail.com>
2023-12-12 14:07:04 +0200

    fix: Script to create a template from the arguments.

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // Script that print a template created from arguments.
 
-let arg1 = process.argv[2];
-let arg2 = process.argv[3];
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
 console.log(arg1 + ' is ' + arg2);
