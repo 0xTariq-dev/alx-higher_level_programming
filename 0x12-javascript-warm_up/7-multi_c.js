8baeb85422dea32442ca3bf9c0c4d5ed881b3eca
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2023-12-12 19:38:14 +0200

    feat: Script to print 'C is fun' a given number of times.

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Script that print 'C is fun' a given number of times.
+if (!isNaN(process.argv[2])) {
+  const x = parseInt(process.argv[2]);
+  for (let i = 0; i < x; i++) { console.log('C is fun'); }
+} else {
+  console.log('Missing number of occurrences');
+}
