121e69e1f7f5104ab5f8a48215d572a80e39c14e
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2023-12-12 19:22:46 +0200

    feat: Script to loop over array elements and print it.

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 // Script that Check if the argument is a number then parse and print it.
-const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 strings.forEach((element) => { console.log(element); });
