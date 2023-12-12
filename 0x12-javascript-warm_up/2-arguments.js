95468336d290e1e65239cdcdfcfb6a5268447901
0xTariq-dev <tariqmohsen222@gmail.com>
2023-12-12 13:35:40 +0200

    fix: Script that check for command line arguments.

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
 // Script that Check the existance of command-line arguments.
-
-const { argv } = require('node:process');
-
-if (argv.length === 2) {
+if (process.argv.length === 2) {
   console.log('No argument');
-} else if (argv.length === 3) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
