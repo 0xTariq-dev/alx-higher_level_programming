4a6199d31bd007626d283e028ca1104ebb5fdb8e
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2023-12-12 23:07:09 +0200

    feat: Script to call and increment a function.

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 // Script that adds two numbers.
-exports.addMeMaybe = function (number, theFunction) { theFunction (number + 1); }
+exports.addMeMaybe = function (number, theFunction) { theFunction(number + 1); };
