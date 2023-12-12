a3ef9a7959a0d8d0125d70e37360dd66fc7652eb
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2023-12-12 20:49:38 +0200

    feat: Script to find the factorial of a number.

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Script that to calculate factorial of a number.
+function factorial (x) {
+    if (x < 0) { return (-1) };
+    if (x === 0 || isNaN(x)) { return (1); }
+    return (x * factorial(x - 1));
+}
+console.log(factorial(parseInt(process.argv[2])));
