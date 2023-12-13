88e399055203f3e2bce68d6ac6a9bd1d561d3083
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2023-12-13 22:56:45 +0200

    fix: Script 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,7 +3,7 @@
 const dict = require('./101-data.js').dict;
 // using reduce function with argument acc to accumulate object.
 const newDict = Object.entries(dict).reduce((acc, [key, value]) => {
-  // check if value exists a key and push value if not create a new key.  
+  // check if value exists a key and push value if not create a new key.
   acc[value] ? acc[value].push(key) : acc[value] = [key];
   // return the accumulator object.
   return acc;
