131700cdcf467970cc5a9330495bba322c3025d9
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2023-12-13 22:55:32 +0200

    feat: script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence.

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const dict = require('./101-data.js').dict;
+// using reduce function with argument acc to accumulate object.
+const newDict = Object.entries(dict).reduce((acc, [key, value]) => {
+  // check if value exists a key and push value if not create a new key.  
+  acc[value] ? acc[value].push(key) : acc[value] = [key];
+  // return the accumulator object.
+  return acc;
+}, {});
+console.log(newDict);
