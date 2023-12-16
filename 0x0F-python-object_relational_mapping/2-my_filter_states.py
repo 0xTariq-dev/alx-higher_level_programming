847c63b78c3d5a294f835d1c61d7bb4ea0cb64c7
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2023-12-16 02:08:51 +0200

    feat: Add Script to get all states where name matches an argument.

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module: 0-select_states"""
+
+import MySQLdb
+from sys import argv
+
+conn = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                       passwd=argv[2], db=argv[3])
+
+cur = conn.cursor()
+cur.execute("SELECT * FROM states WHERE name = %s;", (argv[4],))
+states = [row for row in cur.fetchall()]
+print(*states, sep='\n')
