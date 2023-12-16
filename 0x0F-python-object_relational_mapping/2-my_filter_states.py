ca7e52eb23fd8fdd45b9a2d1e53929ab3dc89c47
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2023-12-16 04:07:47 +0200

    feat: Add script that lists all cities joined by state name.

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""Module: 0-select_states"""
+"""Module: 2-my_filter_states"""
 
 import MySQLdb
 from sys import argv
 
-conn = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
-                       passwd=argv[2], db=argv[3])
+if __name__ == '__main__':
+    conn = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                        passwd=argv[2], db=argv[3])
 
-cur = conn.cursor()
-cur.execute("SELECT * FROM states WHERE name = '{}';".format(argv[4]))
-states = [row for row in cur.fetchall()]
-print(*states, sep='\n')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '{}';".format(argv[4]))
+    states = [row for row in cur.fetchall()]
+    print(*states, sep='\n')
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,6 +9,9 @@ if __name__ == '__main__':
                         passwd=argv[2], db=argv[3])
 
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states;")
-    states = [row for row in cur.fetchall()]
-    print(*states, sep='\n')
+    cur.execute("SELECT cities.id, cities.name, states.name\
+                FROM cities\
+                LEFT JOIN states\
+                ON cities.state_id = states.id;")
+    cities = [row for row in cur.fetchall()]
+    print(*cities, sep='\n')
