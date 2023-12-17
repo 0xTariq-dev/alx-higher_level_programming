775b0e49417fb4805df8833819d950f0ef4fb791
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2023-12-17 22:30:37 +0200

    feat: 16. List relationship task.

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Script to prints all City objects from the database hbtn_0e_14_usa"""
+
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    mk_session = sessionmaker(bind=engine)
+    session = mk_session()
+    for st in session.query(State).order_by(State.id):
+        print(f"{st.id}: {st.name}")
+        for ct in session.query(City).filter(st.id == City.state_id):
+            print(f"\t{ct.id}: {ct.name}")
