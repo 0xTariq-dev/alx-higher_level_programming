c69a3d3c587ac51793aeaca943db72100463e24d
0xTariq-dev <tariqmohsen222@gmail.com>
2023-12-17 12:55:59 +0200

    fix: 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,6 +9,9 @@ if __name__ == '__main__':
                                 passwd=argv[2], db=argv[3])
 
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states;")
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
     states = [row for row in cur.fetchall()]
     print(*states, sep='\n')
+    cur.close()
+    conn.close()
