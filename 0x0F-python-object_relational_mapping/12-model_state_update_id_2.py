71ae45dcf7c402bcc963b68d00ae823eaa0c6f39
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2023-12-17 20:13:23 +0200

    feat: Add Script to update the name of the state with id 2.

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Script to add state 'Louisiana' to states table"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    mk_session = sessionmaker(bind=engine)
+    session = mk_session()
+    session.query(State).filter(State.id == 2).update({'name': 'New Mexico'})
+    session.commit()
