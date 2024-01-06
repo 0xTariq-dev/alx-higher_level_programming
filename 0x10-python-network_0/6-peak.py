4fa5b2c04b6feb82d38a114319ace9c585b2e78f
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-01-06 19:51:22 +0200

    feat: Add method to find the peak in an unsorted array.

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# Defining a function that finds a find_peak in a list of unsorted integers.
+
+def find_peak(IntList):
+    """Function that finds a peak in a list of unsorted integers."""
+    if IntList is None or IntList == []:
+        return None
+    start, end = 0, len(IntList)
+    mid = int(((end - start) // 2) + start)
+
+    if end <= 1:
+        return IntList[0]
+    if end == 2:
+        return max(IntList)
+    if IntList[mid] > IntList[mid - 1] and IntList[mid] > IntList[mid + 1]:
+        return IntList[mid]
+    if mid > 0 and IntList[mid] <= IntList[mid + 1]:
+        return find_peak(IntList[mid + 1:])
+    if mid > 0 and IntList[mid] <= IntList[mid - 1]:
+        return find_peak(IntList[:mid - 1])
