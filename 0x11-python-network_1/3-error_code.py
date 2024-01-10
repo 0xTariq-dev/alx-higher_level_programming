2f3d43fcbc09981d4a9106fb7e2d474f4d244112
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-01-10 19:24:39 +0200

    fix: Script that sends a request to the URL and displays the body of the response.

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 """sends a request to the passed URL and displays the body of the response"""
-from urllib import request, response, error
+from urllib import request, error
 from sys import argv
 
 
 if __name__ == "__main__":
     # Create a request object
-    req = request.Request(argv[1])
     try:
-        request.urlopen(req)
+        with request.urlopen(argv[1]) as r:
+            print(r.read().decode("utf-8"))
     except error.HTTPError as e:
-        if hasattr(e, 'code'):
-            print("Error code: {}".format(e.code))
-    else:
-        print("Index")
+        print(f"Error code: {e.code}")
