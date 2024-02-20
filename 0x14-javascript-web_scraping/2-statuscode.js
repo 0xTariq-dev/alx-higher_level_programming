0bb7df7a4a02a095be4a0f08deed0f323995ca5f
Tariq Abu-Elhamd <tariqmohsen222@gmail.com>
2024-02-20 19:48:15 +0200

    fix: Script to print a status code of a GET request.

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,7 +5,8 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (err, response) {
-  if (response) {
-    console.log('code:', response.statusCode);
+  if (err) {
+    console.log(err);
   }
+  console.log('code:', response.statusCode);
 });
